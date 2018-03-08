500d17eacc597a0752a73cdd6d0baf976266f086
Eden <emesfun@gmail.com>
2018-03-08 14:35:53 -0500

    creating #events to scrape event info from Technical.ly site

--- cli_app/lib/cli_app_folder/articles.rb
+++ cli_app/lib/cli_app_folder/articles.rb
@@ -16,7 +16,7 @@ class Articles #CLIApp::Articles
   end
 
 
-  def self.today
+  def self.articles
     website = HTTParty.get("https://technical.ly/dc/")
     technically = Nokogiri::HTML(website)
     technically.css(".network-post-link")
@@ -25,7 +25,7 @@ class Articles #CLIApp::Articles
         unless article.blank?
           url = article.attribute("href").value
           name = article.attribute("title").value.gsub("Read more about ", "")
-          @@articles << [self.new(name, url)]
+          @@articles << self.new(name, url)
         end #unless loop
     end #each iteration
 
@@ -34,18 +34,51 @@ class Articles #CLIApp::Articles
       unless article.blank?
         url = article.attribute("href").value
         name = article.attribute("title").value.gsub("Read more about ", "").gsub("\u2019", "'")
-        @@articles << [self.new(name, url)]
+        @@articles << self.new(name, url)
       end #unless loop
     end #each iteration
 
-    @@articles.uniq
+    @@articles = @@articles.uniq
 
   end #today method
-
-  def self.coding_puns
-  end
-
 
 
 
 end #class
+
+class Events
+
+  attr_accessor :event_name, :address, :date, :description
+
+  @@events = []
+
+  def initialize (event_name, date, address, description)
+    @event_name = event_name
+    @date = date
+    @address = address
+    @description = description
+  end
+
+
+  def self.events
+    events_website = HTTParty.get("https://technical.ly/events/")
+    events = Nokogiri::HTML(events_website)
+    events.css(".job-listing").each do |listing|
+
+      event_name = listing.children.first.text
+      address = listing.children[2].text
+      date = listing.children[3].text
+      binding.pry
+      description =  listing.children[4].text
+      @@events << self.new(event_name, date, address, description)
+    end #each iteration
+    @@events.uniq
+    binding.pry
+  end #events method
+
+end #class events
+
+
+#listing.children.first
+
+Events.events
--- cli_app/lib/cli_app_folder/cli.rb
+++ cli_app/lib/cli_app_folder/cli.rb
@@ -4,11 +4,9 @@ require_relative 'articles'
 
 class Initialize #CLIApp::CLI
 
-  @@basket = []
-
-  @@articles = Articles.today
-
   def call
+    @articles = Articles.articles
+    @basket = []
     welcome
   end
 
@@ -22,7 +20,7 @@ class Initialize #CLIApp::CLI
   end
 
   def basket
-    print @@basket.uniq
+    print @basket.uniq
   end
 
   def welcome
@@ -30,46 +28,40 @@ class Initialize #CLIApp::CLI
     puts "| ********************************************** |"
     puts "| ********************************************** |"
     puts "| ********************************************** |"
-    puts "| *********WELCOME TO TECHNICAL.lY DC!!!!******* |"
+    puts "| *********WELCOME TO TECHNICAL.LY!!!!********** |"
     puts "| ********************************************** |"
     puts "| ********************************************** |"
     puts "| ********************************************** |"
     puts "-------------------------------------------------"
 
+    puts "Are you ready to get your read ON?"
+    puts "Carpe librum!"
+
     welcome_message
   end
 
   def welcome_message
-    puts "Are you ready to get your read ON?"
-    puts "Carpe librum!"
 
-    puts "Please type LIST to receive the complete list of articles."
+    puts "Please type ARTICLES to receive the complete list of articles."
     puts "From there, you will be able to select an article."
-    puts "If you would like to exit, type LIST. When the list loads, type EXIT."
+    puts "If you would like to exit, type ARTICLES. When the list loads, type EXIT."
 
-    input = nil
-    while input != "LIST"
       input = gets.chomp
-      if input != "LIST"
+      if input != "EXIT" && input != "ARTICLES"
         puts "Uh-oh, I can't compute."
-        puts "Please type LIST to receive the complete list of articles."
-        puts "From there, you will be able to select an article."
-        puts  "If you would like to exit, type LIST. When the list loads, type EXIT."
+        welcome_message
+      elsif input == "ARTICLES"
+        library
       end
-    end #while loop
-    library
+      goodbye
   end
 
   def list_articles
     puts "Check out these articles:"
-    @@articles = Articles.today #CLIApp::Articles.today
-    @@articles.each.with_index(1) do |array, i|
+    @articles.each.with_index(1) do |article, i|
         puts "#{i}."
-      array.each.with_index do |article, i|
         puts "Title: #{article.name}"
         puts "URL: #{article.url}"
-
-      end #each iteration
     end #each iteration
   end
 
@@ -78,30 +70,27 @@ class Initialize #CLIApp::CLI
     input = nil
     while input != "EXIT"
       puts "Please enter the number of the article you would like to read,"
-      puts "type LIST to receive the complete list of articles,"
+      puts "type ARTICLES to receive the complete list of articles,"
       puts "type BASKET to receive the list of articles you selected,"
       puts "or type EXIT to leave."
       input = gets.chomp
-        if input == "LIST"
+        if input == "ARTICLES"
           list_articles
         elsif input == "BASKET"
           basket
         elsif input.to_i - 1 < 0 && input != "EXIT"
           puts "I'm not sure what you mean by that."
-        elsif input.to_i - 1 >= @@articles.length && input != "EXIT"
+        elsif input.to_i - 1 >= @articles.length && input != "EXIT"
           puts "I'm not sure what you mean by that."
-        else input.to_i > 0 && input.to_i - 1 <= @@articles.length && input != "EXIT"
-            the_article = @@articles[input.to_i - 1]
-            the_article.each.with_index(1) do |article, i|
-                puts "Article: #{article.name}."
-                puts "URL: #{article.url}"
+        elsif input.to_i > 0 && input.to_i - 1 <= @articles.length && input != "EXIT"
+            the_article = @articles[input.to_i - 1]
+                puts "Article: #{the_article.name}."
+                puts "URL: #{the_article.url}"
 
-                @@basket << "Article: #{article.name}, URL: #{article.url}"
+                @basket << "Article: #{the_article.name}, URL: #{the_article.url}"
 
-            end #each with index
         end #if statement
     end #while loop
-    goodbye
   end #selection method
 
 
