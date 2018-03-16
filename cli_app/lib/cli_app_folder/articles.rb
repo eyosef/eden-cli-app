aea551f5f7ea469730c451dc2aeb7d64f1f6c5ff
Eden <emesfun@gmail.com>
2018-03-16 15:31:18 -0400

    refactoring class method articles

--- cli_app/lib/cli_app_folder/articles.rb
+++ cli_app/lib/cli_app_folder/articles.rb
@@ -15,12 +15,19 @@ class Articles #CLIApp::Articles
     @url = url
   end
 
-
   def self.articles
     website = HTTParty.get("https://technical.ly/dc/")
     technically = Nokogiri::HTML(website)
     technically.css(".network-post-link")
 
+    newest_posts
+    latest_posts
+
+    @@articles = @@articles.uniq
+
+  end #today method
+
+  def newest_posts
     technically.css(".network-posts").children.each do |article|
         unless article.blank?
           url = article.attribute("href").value
@@ -28,8 +35,9 @@ class Articles #CLIApp::Articles
           @@articles << self.new(name, url)
         end #unless loop
     end #each iteration
+  end
 
-
+  def latest_posts
     technically.css(".latest-post-link").each do |article| #technically.css(".latest-posts-container a") #technically.css(".latest-posts a")
       unless article.blank?
         url = article.attribute("href").value
@@ -37,62 +45,8 @@ class Articles #CLIApp::Articles
         @@articles << self.new(name, url)
       end #unless loop
     end #each iteration
-
-    @@articles = @@articles.uniq
-
-  end #today method
+  end
 
 
 
 end #class
-#
-# class Events
-#
-#   attr_accessor :event_name, :address, :date, :description
-#   @@final_events = []
-#
-#   @@events = []
-#
-#   def initialize(event_name, date, address, description)
-#     @event_name = event_name
-#     @date = date
-#     @address = address
-#     @description = description
-#   end
-#
-#
-#   def self.events
-#     events_website = HTTParty.get("https://technical.ly/events/")
-#     events = Nokogiri::HTML(events_website)
-#     events.css(".job-listing").each do |listing|
-#
-#         event_name = listing.children.first.text
-#         date =  listing.children.children[3].text
-#         event_name = listing.children.first.text
-#         address = listing.children[2].text
-#         description =  listing.children[4].text
-#
-#
-#       # event_name = listing.children.first.text
-#       # address = listing.children[2].text
-#       # date = listing.children[3].text
-#       #description =  listing.children[4].text
-#
-#       @@events << [self.new(event_name, date)]
-#
-#       @@final_events << @@events[0]
-#       @@final_events << @@events[5]
-#       @@final_events << @@events[7]
-#       @@final_events << @@events[8]
-#
-#
-#     end #each iteration
-#     @@events.uniq
-#     binding.pry
-#
-#   end #events method
-#
-# end #class events
-
-
-#listing.children.first
